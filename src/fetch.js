5433a448c5b011f225f9add3d45b5eca98b64ebb
Inna Makarenko <macksi@ukr.net>
2023-04-02 01:43:28 +0300

    update

--- src/fetch.js
+++ src/fetch.js
@@ -1,10 +1,14 @@
+import axios from 'axios';
+
 const BASE_URL = 'https://pixabay.com/api/?key=';
 const API_KEY = '34934818-9a58b99072f8bb0bce42e5818';
-let searchTerm = '';
+// let searchTerm = '';
+let pageNumber = 1;
+const perPage = 40;
 
-export const getImage = () =>
+export const getImage = (searchTerm, pageNumber) =>
   fetch(
-    `${BASE_URL}${API_KEY}&q=${searchTerm}&image_type=photo&orientation=horizontal&safesearch=true`,
+    `${BASE_URL}${API_KEY}&q=${searchTerm}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${perPage}&page=${pageNumber}`,
     { method: 'GET' }
   ).then(res => {
     if (!res.ok) {
@@ -12,4 +16,7 @@ export const getImage = () =>
     }
     return res.json();
   });
-
+// export const getImage = (searchTerm, pageNumber) =>
+//   { return axios.get(
+//     `${BASE_URL}${API_KEY}&q=${searchTerm}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${perPage}&page=${pageNumber}`
+//   )};
--- src/index.js
+++ src/index.js
@@ -1,16 +1,117 @@
 import { getImage } from './fetch';
 import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
+const divGallery = document.querySelector('.gallery');
+const formEl = divGallery.previousElementSibling;
+const loadMoreBtn = divGallery.nextElementSibling;
+let pageNumber = 1;
+// let lightbox;
 
-const formEl = document.querySelector('#search-form');
-const divGallery = formEl.nextElementSibling;
-console.log("🚀 ~ file: index.js:4 ~ divGallery:", divGallery)
+loadMoreBtn.classList.add('is-hidden');
 
 formEl.addEventListener('submit', handleSearchImg);
 
 function handleSearchImg(e) {
     e.preventDefault();
-   searchTerm = formEl.elements.searchQuery.value;
-    getImage().then(data => { console.log(data); console.log("🚀searchQuery.value:", searchTerm); }).catch(console.warn);
+    
+  let lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+  });
+    
+  let searchTerm = '';
+  searchTerm = formEl.elements.searchQuery.value;
+
+  getImage(searchTerm, pageNumber).then(({ hits, totalHits }) => {
+    divGallery.innerHTML = '';
+    if (hits.length === 0) {
+      Notiflix.Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+    }
+    totalHits = totalHits;
+    Notiflix.Notify.info(`"Hooray! We found ${totalHits} images."`);
+    hits.map(item => {
+      markupGallery(item);
+    });
+    loadMoreBtn.classList.toggle(
+      'is-hidden',
+      pageNumber >= Math.ceil(totalHits / 40)); // ховаємо кнопку, якщо більше немає картинок
+  });
+  loadMoreBtn.addEventListener('click', handleLoadMore);
+  function handleLoadMore() {
+    pageNumber += 1;
+    getImage(searchTerm, pageNumber)
+      .then(({ hits, totalHits }) => {
+        if (pageNumber >= Math.ceil(totalHits / 40)) {
+          loadMoreBtn.classList.add('is-hidden');
+          Notiflix.Notify.info(
+            "We're sorry, but you've reached the end of search results."
+          );
+        } else {
+          hits.map(item => {
+            markupGallery(item);
+          });
+        }
+        lightbox.refresh();
+        const { height: cardHeight } = document
+          .querySelector('.gallery')
+          .lastElementChild.getBoundingClientRect();
+
+        window.scrollBy({
+          top: cardHeight * 2,
+          behavior: 'smooth',
+        });
+      })
+      .catch(console.warn);
+  }
+
+  //       var lightbox = $('.gallery a', {
+  //     captionsData: 'alt',
+  //     captionPosition: 'bottom',
+  //     captionDelay: 250,
+  //   }).simpleLightbox();
+    
+  // let lightbox = new SimpleLightbox('.gallery a', {
+  //     captionsData: 'alt',
+  //     captionPosition: 'bottom',
+  //     captionDelay: 250,
+  //   });
 }
 
+function markupGallery({
+  webformatURL,
+  largeImageURL,
+  tags,
+  likes,
+  views,
+  comments,
+  downloads,
+}) {
+  divGallery.insertAdjacentHTML(
+    'beforeend',
+    `
+  <a class = "gallery__item" href="${largeImageURL}">
+  <div class="photo-card">
+  <img class = "gallery__image" src="${webformatURL}" alt="${tags}" loading="lazy" title="${tags}" />
+  <ul class="info">
+    <li class="info-item"><p>
+      <b>Likes ${likes}</b>
+    </p></li>
+    <li class="info-item"><p>
+      <b>Views ${views}</b>
+    </p></li>
+    <li class="info-item"><p>
+      <b>Comments ${comments}</b>
+    </p></li>
+    <li class="info-item"><p>
+      <b>Downloads ${downloads}</b>
+    </p></li>
+  </ul>
+  </div>
+  </a>`
+  );
+}
